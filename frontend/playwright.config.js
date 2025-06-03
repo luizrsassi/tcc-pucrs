a952b37b465364463798136014915dbf6a482fd4
luizrsassi <luizrenato171@gmail.com>
2025-06-03 14:59:41 -0300

    mudança na configuração do playwright

--- frontend/playwright.config.js
+++ frontend/playwright.config.js
@@ -30,4 +30,11 @@ export default defineConfig({
 
   ],
 
+  webServer: {
+    command: 'npm run dev',
+    url: 'http://localhost:5173/',
+    reuseExistingServer: !process.env.CI,
+    timeout: 120 * 1000,
+  },
+
 });
