d8ccf28c8597e91e7d58ad753e3fbe830212fcb3
luizrsassi <luizrenato171@gmail.com>
2025-06-04 14:51:13 -0300

    mudança nos nomes dos jobs

--- frontend/tests/login.spec.js
+++ frontend/tests/login.spec.js
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-test.describe('Testes de Login', () => {
+test.describe.serial('Testes de Login', () => {
     test.beforeEach(async ({ page }) => {
         await page.goto('/login');
     });
